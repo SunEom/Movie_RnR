19031bc6059430fe09cb9cdd89f6631d9250d3c0
seongmin <psm7335@gmail.com>
2021-02-01 19:43:43 +0900

    essage2

---
+++ server/lib/db.js
@@ -0,0 +1,9 @@
+var mysql = require('mysql');
+var db = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'psm',
+    password : '111111',
+    database : 'movie_rnr'
+  });
+  db.connect();
+  module.exports = db;
