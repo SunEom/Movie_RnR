ac8800af9abcaebffdf34a05c17d1d1f90107298
suneom <dkxl0701@gmail.com>
2021-02-01 17:16:30 +0900

    Created Navigation component

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Home</li>
+        <li>Create</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
