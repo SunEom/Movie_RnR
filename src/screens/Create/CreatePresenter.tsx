786f2511cf63bb3869cb3e6c6482936d07daf96d
suneom <dkxl0701@gmail.com>
2021-02-01 20:10:10 +0900

    Created Create screen logic

--- src/screens/Create/CreatePresenter.tsx
+++ src/screens/Create/CreatePresenter.tsx
@@ -3,16 +3,30 @@ import Navigation from '../../components/Navigation';
 import { TextField, Button } from '@material-ui/core';
 import './Create.css';
 
-const Create = () => {
+type CreateProps = { onChange: (e: any) => void; onSubmit: (e: any) => void };
+
+const Create = ({ onChange, onSubmit }: CreateProps) => {
   return (
     <div className="main__screen">
       <div className="main__screen__content">
-        <form className="post__form" noValidate autoComplete="off">
+        <form className="post__form" noValidate autoComplete="off" onSubmit={onSubmit}>
           <div className="post__form__input">
-            <TextField id="standard-basic" label="Title" style={{ minWidth: '10rem', width: '25%' }} inputProps={{ name: 'title' }} />
+            <TextField
+              id="standard-basic"
+              label="Title"
+              style={{ minWidth: '10rem', width: '25%' }}
+              inputProps={{ name: 'title', onChange }}
+              required
+            />
           </div>
           <div className="post__form__input">
-            <TextField id="standard-basic" label="Genres" style={{ minWidth: '6rem', width: '12%' }} inputProps={{ name: 'genres' }} />
+            <TextField
+              id="standard-basic"
+              label="Genres"
+              style={{ minWidth: '6rem', width: '12%' }}
+              inputProps={{ name: 'genres', onChange }}
+              required
+            />
           </div>
           <div className="post__form__input">
             <TextField
@@ -20,7 +34,8 @@ const Create = () => {
               type="number"
               label="Rates / 10"
               style={{ minWidth: '6rem', width: '12%' }}
-              inputProps={{ max: 10, step: 0.1, name: 'rates' }}
+              inputProps={{ min: 0, max: 10, step: 0.1, name: 'rates', onChange }}
+              required
             />
           </div>
           <div className="post__form__input post__form__input__overview">
@@ -32,11 +47,12 @@ const Create = () => {
               defaultValue="This movie is ..."
               variant="outlined"
               style={{ width: '100%' }}
-              inputProps={{ name: 'overview' }}
+              inputProps={{ name: 'overview', onChange }}
+              required
             />
           </div>
           <div className="post__form__submit__btn">
-            <Button variant="contained" color="primary" size="small">
+            <Button variant="contained" color="primary" size="small" type="submit">
               Save
             </Button>
           </div>
