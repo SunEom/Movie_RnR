ed85ff68ac182a7a7694ac8f22cb3955081fd624
suneom <dkxl0701@gmail.com>
2021-02-03 15:33:09 +0900

    Update to check rates

--- server/routes/topic.js
+++ server/routes/topic.js
@@ -5,42 +5,42 @@ const sanitizeHtml = require('sanitize-html');
 const db = require('../lib/db');
 var qs = require('querystring');
 
-router.post('/create_process', function(req,res){
-    var post = req.body;
-    db.query(
-        `INSERT INTO movie(title,description,genre,created)
+router.post('/create_process', function (req, res) {
+  var post = req.body;
+  db.query(
+    `INSERT INTO movie(title,description,genre,created)
         VALUES(?,?,NOW(),?);`,
-        [post.title,post.description,post.genre],
-        function(error,result){
-            if(error){
-                throw error;
-            }
-            res.redirect(`/topic/?id=${result.insertId}`);
-        }
-    );
+    [post.title, post.description, post.genre],
+    function (error, result) {
+      if (error) {
+        throw error;
+      }
+      res.redirect(`/topic/?id=${result.insertId}`);
+    }
+  );
 });
 
-router.post('/update_process',function(req,res){
-    var post = req.body;
-    db.query(
-        `UPDATE movie SET title=?, description=?, genre=? WHERE id=?`
-        ,[post.title,post.description,post.genre,post.id],
-        function(error,result){
-            if(error){
-                throw error;
-            }
-            res.redirect(`/topic/?id=${post.id}`);
-        }
-    );
+router.post('/update_process', function (req, res) {
+  var post = req.body;
+  db.query(
+    `UPDATE movie SET title=?, description=?, genre=? WHERE id=?`,
+    [post.title, post.description, post.genre, post.id],
+    function (error, result) {
+      if (error) {
+        throw error;
+      }
+      res.redirect(`/topic/?id=${post.id}`);
+    }
+  );
 });
 
-router.post('/delete_process', function(req,res){
-    var post = request.body;
-    db.query(`DELETE FROM movie WHERE id = ?`,[post.id],function(error,result){
-        if(error){
-            throw error;
-        }
-        res.redirect('/');
-    });
+router.post('/delete_process', function (req, res) {
+  var post = request.body;
+  db.query(`DELETE FROM movie WHERE id = ?`, [post.id], function (error, result) {
+    if (error) {
+      throw error;
+    }
+    res.redirect('/');
+  });
 });
 module.exports = router;
