56d9c7d732b8290e8b5e486fa86e11fb6479f0e4
suneom <dkxl0701@gmail.com>
2021-02-03 18:04:11 +0900

    Update join logic

--- src/screens/Join/JoinPresenter.tsx
+++ src/screens/Join/JoinPresenter.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const Home = () => {
+type JoinProps = { onChange: (e: any) => void; onSubmit: (e: any) => void };
+
+const Join = ({ onSubmit, onChange }: JoinProps) => {
   return (
     <div className="w-screen flex justify-center sm:pt-20">
       <div className="mt-5 md:mt-0 md:col-span-2 w-8/12 sm:w-8/12 md:w-6/12 lg:w-5/12 bg-white bg-opacity-50 rounded-lg">
-        <form action="#" method="POST">
+        <form action="#" method="POST" onSubmit={onSubmit}>
           <div className="shadow overflow-hidden sm:rounded-md">
             <div className="px-4 py-5 ">
               <div className="grid grid-cols-6 gap-6">
@@ -18,6 +20,7 @@ const Home = () => {
                     id="id"
                     className="px-2 mt-1 bg-white h-7 bg-opacity-50 border border-gray-dark focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                     required
+                    onChange={onChange}
                   />
                 </div>
                 <div className="col-span-6 sm:col-span-3"></div>
@@ -31,6 +34,7 @@ const Home = () => {
                     id="password"
                     className="px-2 mt-1 bg-white h-7 bg-opacity-50 border border-gray-dark focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                     required
+                    onChange={onChange}
                   />
                 </div>
 
@@ -44,6 +48,7 @@ const Home = () => {
                     id="password_check"
                     className="px-2 mt-1 h-7 bg-white bg-opacity-50 border border-gray-dark focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                     required
+                    onChange={onChange}
                   />
                 </div>
 
@@ -57,6 +62,7 @@ const Home = () => {
                     id="nickname"
                     className="px-2 mt-1 h-7 bg-white bg-opacity-50 border border-gray-dark focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
                     required
+                    onChange={onChange}
                   />
                 </div>
 
@@ -69,6 +75,7 @@ const Home = () => {
                     name="gender"
                     className="px-2 mt-1 block w-full py-2 px-3 border border-gray-300 bg-white bg-opacity-50 border border-gray-dark rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     required
+                    onChange={onChange}
                   >
                     <option>Man</option>
                     <option>Woman</option>
@@ -91,4 +98,4 @@ const Home = () => {
   );
 };
 
-export default Home;
+export default Join;
