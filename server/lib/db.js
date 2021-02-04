beef4f920493677444b70b8e0f86612656df6eb0
suneom <dkxl0701@gmail.com>
2021-02-04 11:12:33 +0900

    bugfix

--- server/lib/db.js
+++ server/lib/db.js
@@ -1,18 +1,8 @@
-// var mysql = require('mysql');
-// var db = mysql.createConnection({
-//     host     : 'localhost',
-//     user     : 'psm',
-//     password : '111111',
-//     database : 'movie_rnr'
-//   });
-//   db.connect();
-//   module.exports = db;
-
 var mysql = require('mysql');
 var db = mysql.createConnection({
   host: 'localhost',
-  user: 'root',
-  password: 'xodid0701',
+  user: 'psm',
+  password: '111111',
   database: 'movie_rnr',
 });
 db.connect();
