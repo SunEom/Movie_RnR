166a683fdc52eaf0a004a6289f4ec943ec8a7231
suneom <dkxl0701@gmail.com>
2021-02-04 09:52:56 +0900

    Change MovieCard UI

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -10,20 +10,75 @@ type MovieCardProps = {
 
 const MovieCard = ({ id, title, genres, rates }: MovieCardProps) => {
   return (
-    <div className="w-11/12 pt-10 md:w-2/5 lg:w-1/3 px-2 sm:px-6 lg:px-8 ">
-      <div className="overflow-hidden shadow-lg">
-        <div className="px-6 pt-4 pb-2 bg-white border-b border-gray-200 font-bold uppercase whitespace-nowrap">{title}</div>
+    // <div className="w-11/12 pt-10 md:w-2/5 lg:w-1/3 px-2 sm:px-6 lg:px-8 ">
+    //   <div className="overflow-hidden shadow-lg">
+    //     <div className="px-6 pt-4 pb-2 bg-white border-b border-gray-200 font-bold uppercase whitespace-nowrap">{title}</div>
 
-        <div className="px-6 bg-white border-b border-gray">{genres}</div>
-        <div className="px-6 bg-white border-b border-gray">⭐ {rates} / 10</div>
+    //     <div className="px-6 bg-white border-b border-gray">{genres}</div>
+    //     <div className="px-6 bg-white border-b border-gray">⭐ {rates} / 10</div>
 
-        <div className="p-6 bg-white border-gray-200 text-right">
-          <Link
-            className="bg-gray shadow-lg text-sm text-black font-bold py-3 md:px-8 px-4 hover:bg-gray-dark rounded uppercase whitespace-nowrap"
-            to={`/post/${id}`}
+    //     <div className="p-6 bg-white border-gray-200 text-right">
+    //       <Link
+    //         className="bg-gray shadow-lg text-sm text-black font-bold py-3 md:px-8 px-4 hover:bg-gray-dark rounded uppercase whitespace-nowrap"
+    //         to={`/post/${id}`}
+    //       >
+    //         View Detail
+    //       </Link>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="p-4 lg:w-1/3">
+      <div className="h-full bg-gray-dark bg-opacity-40 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+        <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">{genres}</h2>
+        <h1 className="title-font sm:text-2xl text-xl font-medium text-white mb-3">{title}</h1>
+        <p className="leading-relaxed mb-3">
+          Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.
+        </p>
+        <Link className="text-indigo-400 inline-flex items-center" to={`/post/${id}`}>
+          Learn More
+          <svg
+            className="w-4 h-4 ml-2"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            stroke-width="2"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
           >
-            View Detail
-          </Link>
+            <path d="M5 12h14"></path>
+            <path d="M12 5l7 7-7 7"></path>
+          </svg>
+        </Link>
+        <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+          <span className="text-gray-500 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-700 border-opacity-50">
+            <svg
+              className="w-4 h-4 mr-1"
+              stroke="currentColor"
+              stroke-width="2"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              viewBox="0 0 24 24"
+            >
+              <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path>
+              <circle cx="12" cy="12" r="3"></circle>
+            </svg>
+            1.2K
+          </span>
+          <span className="text-gray-500 inline-flex items-center leading-none text-sm">
+            <svg
+              className="w-4 h-4 mr-1"
+              stroke="currentColor"
+              stroke-width="2"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              viewBox="0 0 24 24"
+            >
+              <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+            </svg>
+            6
+          </span>
         </div>
       </div>
     </div>
