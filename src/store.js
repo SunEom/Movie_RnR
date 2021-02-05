df5aa9e7d82acb44bc12392b22e1373c861c96fb
suneom <dkxl0701@gmail.com>
2021-02-05 13:32:07 +0900

    bugfix

--- src/store.js
+++ src/store.js
@@ -1,8 +1,7 @@
 import { createStore } from 'redux';
 
-export default () =>
-  createStore((state, action) => {
-    if (state === undefined) {
-      return { user: null };
-    }
-  }, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+export default createStore((state, action) => {
+  if (state === undefined) {
+    return { user: null };
+  }
+}, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
