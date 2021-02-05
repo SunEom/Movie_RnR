edb13f0118fe83a9ea5e40ad55bd43d271907d44
suneom <dkxl0701@gmail.com>
2021-02-05 13:45:59 +0900

    bugfix : show every sentences in overview

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -16,7 +16,7 @@ const MovieCard = ({ id, title, genres, rates, overview }: MovieCardProps) => {
         <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">{genres}</h2>
         <h1 className="title-font sm:text-2xl text-xl font-semibold text-white mb-3">{title}</h1>
 
-        <p className="leading-relaxed mb-3">{overview.split('.')}.</p>
+        <p className="leading-relaxed mb-3">{overview}</p>
 
         <Link className="text-indigo-400 inline-flex items-center" to={`/post/${id}`}>
           Learn More
