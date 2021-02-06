a6bb522b483de0b843dad8d932ff41af2fd4a7fc
suneom <dkxl0701@gmail.com>
2021-02-06 12:18:04 +0900

    Change login format

--- src/screens/Login/LoginPresenter.tsx
+++ src/screens/Login/LoginPresenter.tsx
@@ -14,13 +14,13 @@ const Home = () => {
           <div className="rounded-md shadow-sm space-y-px pb-2 w-full">
             <div>
               <label htmlFor="email-address" className="sr-only">
-                Email address
+                ID
               </label>
               <input
                 id="email-address"
-                name="email"
-                type="email"
-                autoComplete="email"
+                name="id"
+                type="id"
+                autoComplete="id"
                 required
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
