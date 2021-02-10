a50af3007ed28e4dde495b3a45b4d85ac6c8f008
suneom <dkxl0701@gmail.com>
2021-02-10 17:15:53 +0900

    bugfix : fix delete logic

--- src/screens/Detail/DetailContainer.tsx
+++ src/screens/Detail/DetailContainer.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import DetailPresenter from './DetailPresenter';
 import axios from 'axios';
 import { useHistory } from 'react-router';
+import store from '../../store';
 
 export default ({ id }: { id: string }) => {
   const [movie, setMovie] = useState<object>([{}]);
@@ -17,9 +18,13 @@ export default ({ id }: { id: string }) => {
   const onDeleteClick = (e: any) => {
     e.preventDefault();
     axios
-      .delete(`http://localhost:8000/post/${id}`)
-      .then((response) => history.push({ pathname: '/' }))
-      .catch((err) => console.error(err));
+      .delete(`http://localhost:8000/post/${id}`, { withCredentials: true })
+      .then(async (response) => {
+        const { data } = await axios.get('/post/');
+        store.dispatch({ type: 'GET_RECENT', recent: data });
+        history.push({ pathname: '/' });
+      })
+      .catch((err) => console.error(err.response.data));
   };
 
   useEffect(() => {
