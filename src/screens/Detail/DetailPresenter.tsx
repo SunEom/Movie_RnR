8d711e04eb4483be194f895bcba2aba1696fd37c
suneom <dkxl0701@gmail.com>
2021-02-10 18:03:56 +0900

    Update not founded Detail page

--- src/screens/Detail/DetailPresenter.tsx
+++ src/screens/Detail/DetailPresenter.tsx
@@ -20,7 +20,7 @@ const Detail = ({ movie, loading, onDeleteClick }: DetailProps) => {
     <>
       {loading ? (
         <ActivityIndicator />
-      ) : (
+      ) : movie ? (
         <div className="flex justify-center pt-10">
           <div className=" w-11/12 lg:w-1/2 md:px-4 lg:px-6 py-5">
             <div className="bg-white shadow-xl">
@@ -50,6 +50,23 @@ const Detail = ({ movie, loading, onDeleteClick }: DetailProps) => {
                 >
                   Delete
                 </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="flex justify-center pt-10">
+          <div className=" w-11/12 lg:w-1/2 md:px-4 lg:px-6 py-5">
+            <div className="bg-white shadow-xl">
+              <div className="">
+                <img
+                  src="https://images.pexels.com/photos/956999/milky-way-starry-sky-night-sky-star-956999.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"
+                  alt=""
+                  className="h-56 w-full border-white border-8 hover:opacity-25"
+                />
+              </div>
+              <div className="px-4 py-4 md:px-10">
+                <h1 className="font-bold text-lg">This page is not founded!</h1>
               </div>
             </div>
           </div>
