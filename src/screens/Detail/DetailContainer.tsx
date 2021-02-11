17a4c6950485588bf1d43fcf8ab4829100464043
suneom <dkxl0701@gmail.com>
2021-02-11 21:22:04 +0900

    Created posting editing form

--- src/screens/Detail/DetailContainer.tsx
+++ src/screens/Detail/DetailContainer.tsx
@@ -4,17 +4,115 @@ import axios from 'axios';
 import { useHistory } from 'react-router';
 import store from '../../store';
 
+type post = {
+  id: number;
+  title: string;
+  genres: string;
+  rates: number | undefined;
+  overview: string;
+};
+
 export default ({ id }: { id: string }) => {
-  const [movie, setMovie] = useState<object>([{}]);
+  const [movie, setMovie] = useState<any>([{}]);
   const [loading, setLoading] = useState<boolean>(true);
   const [mode, setMode] = useState('read');
+  const [title, setTitle] = useState<string>('');
+  const [genres, setGenres] = useState<string[]>([]);
+  const [rates, setRates] = useState<number | undefined>(undefined);
+  const [overview, setOverview] = useState<string>('');
   const history = useHistory();
+
+  const onSubmit = async (e: any) => {
+    e.preventDefault();
+
+    if (title === '') {
+      alert('Please input title.');
+      return;
+    }
+    if (genres.length === 0) {
+      alert('Please select at least one genre.');
+      return;
+    }
+    if (rates === undefined) {
+      alert('Please input rates.');
+      return;
+    }
+    if (overview === '') {
+      alert('Please input overview.');
+      return;
+    }
+
+    let _genres = '';
+    for (let i = 0; i < genres.length; i++) {
+      if (i + 1 === genres.length) {
+        _genres += genres[i];
+        break;
+      }
+      _genres += `${genres[i]}, `;
+    }
+
+    const data: post = {
+      id: +id,
+      title,
+      genres: _genres,
+      rates,
+      overview,
+    };
+
+    if ((rates as number) > 10) {
+      alert('Rates must be lower than 10 !');
+      console.log(e.target);
+      return;
+    }
+
+    await axios
+      .post('http://localhost:8000/post/update', { ...data }, { withCredentials: true })
+      .then((response) => {
+        history.push(`/post/${response.data.data[0].id}`);
+      })
+      .catch((err) => console.log('response: ', err.response.data));
+  };
+
+  const onChange = (e: React.FormEvent<HTMLInputElement>) => {
+    switch (e.currentTarget.name) {
+      case 'title': {
+        setTitle(e.currentTarget.value);
+        break;
+      }
+      case 'rates': {
+        if (e.currentTarget.value == '') {
+          setRates(undefined);
+        } else {
+          setRates(+e.currentTarget.value);
+        }
+        break;
+      }
+      case 'overview': {
+        setOverview(e.currentTarget.value);
+        break;
+      }
+    }
+  };
+
+  const onCheck = (e: any) => {
+    if (e.target.checked) {
+      let newItem = e.target.name;
+      setGenres([...genres, newItem] as any);
+    } else {
+      let removedItem = e.target.name;
+      setGenres([...genres].filter((genre) => genre !== removedItem));
+    }
+  };
 
   const getMovie = async () => {
     axios
       .get(`/post/${id}`)
       .then(({ data }) => {
         setMovie(data[0]);
+        setTitle(data[0].title);
+        setGenres(data[0].genres.split(', '));
+        setRates(+data[0].rates);
+        setOverview(data[0].overview);
         setLoading(false);
       })
       .catch((err) => console.error(err));
@@ -43,6 +141,16 @@ export default ({ id }: { id: string }) => {
   }, []);
 
   return (
-    <DetailPresenter movie={movie as any} loading={loading} onDeleteClick={onDeleteClick} mode={mode} modeToggle={modeToggle} user={user} />
+    <DetailPresenter
+      movie={movie as any}
+      loading={loading}
+      onDeleteClick={onDeleteClick}
+      mode={mode}
+      modeToggle={modeToggle}
+      user={user}
+      onChange={onChange}
+      onSubmit={onSubmit}
+      onCheck={onCheck}
+    />
   );
 };
