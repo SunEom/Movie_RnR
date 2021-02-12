c3bae5ff36927c00023d903c3fd8cff5fe9acc1c
suneom <dkxl0701@gmail.com>
2021-02-12 13:17:18 +0900

    Fixed structure of Profile screen

--- src/screens/Profile/ProfileContainer.tsx
+++ src/screens/Profile/ProfileContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProfilePresenter from './ProfilePresenter';
 import { useHistory } from 'react-router';
 import store from '../../store';
@@ -6,9 +6,10 @@ import store from '../../store';
 export default () => {
   const history = useHistory();
   const user = store.getState().user;
+  const [mode, setMode] = useState<'basic' | 'edit' | 'posts'>('basic');
 
   if (!user.user_id) {
     history.push({ pathname: '/' });
   }
-  return <ProfilePresenter user={user} />;
+  return <ProfilePresenter user={user} modeHandler={setMode} mode={mode} />;
 };
--- src/screens/Profile/ProfilePresenter.tsx
+++ src/screens/Profile/ProfilePresenter.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ActivityIndicator from '../../components/ActivityIndicator';
-
+import ProfileNav from '../../components/Profile/ProfileNav';
+import BasicProfile from '../../components/Profile/BasicProfileContainer';
+import EditProfile from '../../components/Profile/EditProfileContainer';
 type ProfileProps = {
   user: any;
+  modeHandler: any;
+  mode: 'basic' | 'edit' | 'posts';
 };
 
-const Profile = ({ user }: ProfileProps) => {
+const Profile = ({ user, modeHandler, mode }: ProfileProps) => {
   return (
     <div className="w-full relative mt-10 rounded my-24 overflow-hidden bg-gray">
       <div className="top h-64 w-full bg-blue-600 overflow-hidden relative">
@@ -24,110 +28,9 @@ const Profile = ({ user }: ProfileProps) => {
         </div>
       </div>
       <div className="grid grid-cols-12 bg-gray ">
-        <div className="col-span-12 w-full px-3 py-6 justify-center flex space-x-4 border-gray border-solid md:space-x-0 md:space-y-4 md:flex-col md:col-span-2 md:justify-start ">
-          <a href="#" className="text-sm p-2 bg-indigo-900 text-white text-center rounded font-bold">
-            Basic Information
-          </a>
-
-          <a href="#" className="text-sm p-2 bg-indigo-200 text-center rounded font-semibold hover:bg-indigo-700 hover:text-gray-200">
-            Another Information
-          </a>
-
-          <a href="#" className="text-sm p-2 bg-indigo-200 text-center rounded font-semibold hover:bg-indigo-700 hover:text-gray-200">
-            Another Something
-          </a>
-        </div>
-
+        <ProfileNav modeHandler={modeHandler} />
         <div className="col-span-12 md:border-solid md:border-l md:border-black md:border-opacity-25 h-full pb-12 md:col-span-10">
-          <div className="px-4 pt-4">
-            <form action="#" className="flex flex-col space-y-8">
-              <div>
-                <h3 className="text-2xl font-semibold">Basic Information</h3>
-              </div>
-
-              <div className="form-item">
-                <label className="text-xl ">ID</label>
-                <input
-                  type="text"
-                  value={user.user_id}
-                  className="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2  mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200"
-                  disabled
-                />
-              </div>
-
-              <div className="flex flex-col space-y-4 md:space-y-0 md:flex-row md:space-x-4">
-                <div className="form-item w-full">
-                  <label className="text-xl ">Nickname</label>
-                  <input
-                    type="text"
-                    value={user.nickname}
-                    className="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2 mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200 text-opacity-25 "
-                    disabled
-                  />
-                </div>
-
-                <div className="form-item w-full">
-                  <label className="text-xl ">Gender</label>
-                  <input
-                    type="text"
-                    value={user.gender}
-                    className="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2 mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200 text-opacity-25 "
-                    disabled
-                  />
-                </div>
-              </div>
-
-              <div>
-                <h3 className="text-2xl font-semibold ">More About Me</h3>
-              </div>
-
-              <div className="form-item w-full">
-                <label className="text-xl ">Biography</label>
-                <textarea
-                  cols={30}
-                  rows={10}
-                  className="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2 mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200 text-opacity-25 "
-                  disabled
-                >
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem natus nobis odio. Lorem ipsum dolor sit amet consectetur
-                  adipisicing elit. Accusantium, eveniet fugiat? Explicabo assumenda dignissimos quisquam perspiciatis corporis sint commodi
-                  cumque rem tempora!
-                </textarea>
-              </div>
-
-              <div>
-                <h3 className="text-2xl font-semibold">My Social Media</h3>
-              </div>
-
-              <div className="form-item">
-                <label className="text-xl ">Instagram</label>
-                <input
-                  type="text"
-                  value="https://instagram.com/"
-                  className="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2 mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200 text-opacity-25 "
-                  disabled
-                />
-              </div>
-              <div className="form-item">
-                <label className="text-xl ">Facebook</label>
-                <input
-                  type="text"
-                  value="https://facebook.com/"
-                  className="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2 mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200 text-opacity-25 "
-                  disabled
-                />
-              </div>
-              <div className="form-item">
-                <label className="text-xl ">Twitter</label>
-                <input
-                  type="text"
-                  value="https://twitter.com/"
-                  className="w-full appearance-none text-black text-opacity-50 rounded shadow py-1 px-2  mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200  "
-                  disabled
-                />
-              </div>
-            </form>
-          </div>
+          {mode === 'basic' ? <BasicProfile /> : <EditProfile />}
         </div>
       </div>
     </div>
