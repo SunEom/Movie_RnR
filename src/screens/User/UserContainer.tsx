8fa1d20a5651f197d1f6812e24b7284fa684e4e8
suneom <dkxl0701@gmail.com>
2021-02-15 17:38:13 +0900

    Updated Other user's profile screen

--- src/screens/User/UserContainer.tsx
+++ src/screens/User/UserContainer.tsx
@@ -8,23 +8,23 @@ const UserContainer = () => {
   const [loading, setLoading] = useState(true);
   const [mode, setMode] = useState<'basic' | 'posts'>('basic');
   const [user, setUser] = useState({ id: 5, user_id: 'zxcvzxcv', nickname: 'zxcvzxcv', gender: 'Woman' });
-  const params = useParams();
+  const params: any = useParams();
 
   const getUser = () => {
-    // axios
-    //   .get(`http://localhost:8000/user/${params.id}`, { withCredentials: true })
-    //   .then((response) => {
-    //     setUser(response.data.data);
-    //     setLoading(false);
-    //   })
-    //   .catch((err) => console.error(err));
+    axios
+      .get(`http://localhost:8000/user/${params.id}`, { withCredentials: true })
+      .then((response) => {
+        setUser(response.data.data[0]);
+        setLoading(false);
+      })
+      .catch((err) => console.error(err));
   };
 
   useEffect(() => {
     getUser();
   }, []);
 
-  return <UserPresenter modeHandler={setMode} mode={mode} user={user} />;
+  return <UserPresenter modeHandler={setMode} mode={mode} user={user} loading={loading} />;
 };
 
 export default UserContainer;
