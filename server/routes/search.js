164a12209e0651839ccddca8a1bbbffda590225c
seongmin <psm7335@gmail.com>
2021-02-16 18:24:37 +0900

    fix search

--- server/routes/search.js
+++ server/routes/search.js
@@ -7,7 +7,7 @@ const qs = require('querystring');
 router.post('/', async function (req, res, next) {
     const post = req.body;
     let keyword = "%" + post.keyword + "%";
-    await db.query(`SELECT * FROM movie WHERE title LIKE ? OR overview LIKE ?`,
+    await db.query(`select count(comment.id) as commentCount, M.id, M.title, M.overview, M.created, M.genres, M.rates, M.updated, M.user_id FROM comment right join(select * FROM movie WHERE title LIKE ? OR overview LIKE ?) as M on (comment.movie_id = M.id) group by movie_id;`,
     [keyword,keyword],
     function(error,result){
         if(error){
