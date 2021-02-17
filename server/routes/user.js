9580339593fad055c3dd740a88cbf299c738095c
suneom <dkxl0701@gmail.com>
2021-02-17 16:47:00 +0900

    Created Change Password page

--- server/routes/user.js
+++ server/routes/user.js
@@ -12,29 +12,31 @@ router.post('/profile', async function (req, res, next) {
   }
   const post = req.body;
   console.log(req.user.id);
-  await db.query(`UPDATE user SET nickname=?, gender=? WHERE id=?`,
-    [post.nickname, post.gender, req.user.id],
-    async (error, result) => {
-      if (error) {
-        next(err);
-      }
-      await db.query(`UPDATE aboutme SET biography=?, instagram=? ,facebook=? ,twitter=? WHERE id=?`,
-      [post.biography, post.instagram, post.facebook, post.twitter, req.user.aboutId], 
-      async function(error2,result2){
-        if(error2){
+  await db.query(`UPDATE user SET nickname=?, gender=? WHERE id=?`, [post.nickname, post.gender, req.user.id], async (error, result) => {
+    if (error) {
+      next(error);
+    }
+    await db.query(
+      `UPDATE aboutme SET biography=?, instagram=? ,facebook=? ,twitter=? WHERE id=?`,
+      [post.biography, post.instagram, post.facebook, post.twitter, req.user.aboutId],
+      async function (error2, result2) {
+        if (error2) {
           next(error2);
         }
-        await db.query(`SELECT user.id,user_id,password,nickname,gender,aboutme.id AS aboutId,biography,instagram,facebook,twitter,my_id FROM user LEFT JOIN aboutme on user.id=aboutme.my_id WHERE user.id=?`, [req.user.id], async (error3, result3) => {
-          if (error3) {
-            next(error3);
+        await db.query(
+          `SELECT user.id,user_id,password,nickname,gender,aboutme.id AS aboutId,biography,instagram,facebook,twitter,my_id FROM user LEFT JOIN aboutme on user.id=aboutme.my_id WHERE user.id=?`,
+          [req.user.id],
+          async (error3, result3) => {
+            if (error3) {
+              next(error3);
+            }
+            res.status(201).send({ code: 201, data: result3 });
           }
-          res.status(201).send({ code: 201, data: result3 });
-        })
-      })
-    });
+        );
+      }
+    );
+  });
 });
-
-
 
 router.post('/password', async function (req, res, next) {
   //password 변경
@@ -70,15 +72,14 @@ router.post('/password', async function (req, res, next) {
   });
 });
 
-router.get('/:id', async function (req, res, next){
-  await db.query(`SELECT id, nickname, gender FROM user WHERE id=?`,[req.params.id],
-  function(error,result){
+router.get('/:id', async function (req, res, next) {
+  await db.query(`SELECT id, nickname, gender FROM user WHERE id=?`, [req.params.id], function (error, result) {
     if (error) {
       next(error);
     }
-    console.log("프로필",result);
-    res.status(200).send({ code: 200, data: result});
-  })
+    console.log('프로필', result);
+    res.status(200).send({ code: 200, data: result });
+  });
 });
 
 module.exports = router;
---
+++ src/components/Profile/PwdChangePresenter.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+
+type PwdChangePresenterProps = {
+  user: any;
+  password: string;
+  newPassword: string;
+  newPasswordCheck: string;
+  onSubmit: any;
+  onChange: any;
+};
+
+const PwdChangePresenter = ({ user, password, newPassword, newPasswordCheck, onSubmit, onChange }: PwdChangePresenterProps) => {
+  return (
+    <div className="px-4 pt-4">
+      <form action="#" className="flex flex-col space-y-8" onSubmit={onSubmit}>
+        <div>
+          <h3 className="text-2xl font-semibold font-Jua">Change Password</h3>
+        </div>
+
+        <div className="form-item flex flex-col">
+          <label className="text-xl font-Jua">Password</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={onChange}
+            className="w-1/3 appearance-none text-black  rounded shadow py-1 px-2  mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200 "
+          />
+        </div>
+        <div className="form-item flex flex-col">
+          <label className="text-xl font-Jua">New Password</label>
+          <input
+            type="password"
+            id="newPassword"
+            value={newPassword}
+            onChange={onChange}
+            className="w-1/3 appearance-none text-black  rounded shadow py-1 px-2  mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200"
+          />
+        </div>
+        <div className="form-item flex flex-col">
+          <label className="text-xl font-Jua">New Password Check</label>
+          <input
+            type="password"
+            id="newPasswordCheck"
+            value={newPasswordCheck}
+            onChange={onChange}
+            className="w-1/3 appearance-none text-black  rounded shadow py-1 px-2  mr-2 focus:outline-none focus:shadow-outline focus:border-blue-200"
+          />
+        </div>
+
+        <div className="flex justify-end mr-10">
+          <button
+            type="submit"
+            className="btn border border-gray-dark p-1 px-4 font-semibold cursor-pointer text-gray-200 ml-2 bg-gray-dark"
+          >
+            Save
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default PwdChangePresenter;
